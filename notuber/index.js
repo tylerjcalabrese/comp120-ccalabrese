7f5ddaa2f1d65ebed4d04b107fc7531ec85e6638
Camille <camille.calabrese@gmail.com>
2021-06-21 23:31:52 -0400

    Center map on user location

--- notuber/index.js
+++ notuber/index.js
@@ -1,10 +1,26 @@
 let map;
 
-function initMap() {
+var my_latitude, my_longitude;
+
+// callbacks for navigator.geolocation.getCurrentPosition()
+function posSuccess(gpos)
+{
+    userLoc = new google.maps.LatLng(gpos.coords.latitude, gpos.coords.longitude);
+    map.setCenter(userLoc);
+}
+function posError(err) { console.log("there was an error getting pos", err.message); }
+
+function initMap()
+{
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 42.352271, lng: -71.05524200000001 },
+        // map will start at south station in boston, then shift to user's location
+        center: { 
+            lat: 42.352271,
+            lng: -71.05524200000001
+        },
         zoom: 14,
     });
+    navigator.geolocation.getCurrentPosition(posSuccess, posError);
     const carIcon = "https://tuftsdev.github.io/WebEngineering/assignments/summer2021/car.png";
     const cars = 
     [
@@ -52,3 +68,4 @@ function initMap() {
         })
     ];
 }
+
